4d5545296a9cfe9c69dd40d9166472e37cb45cf5
Vova0211 <chernousov_07@bk.ru>
2025-12-01 11:36:31 +0300

    add paths obj to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,32 +2,47 @@ const {src, dest, watch, series, parallel} = require('gulp');
 const cleanCSS = require('gulp-clean-css');
 const uglify = require('gulp-uglify');
 const htmlmin = require('gulp-htmlmin');
-const contact = require('gulp-concat');
+const concat = require('gulp-concat');
 const order = require('gulp-order');
 
+const paths = {
+  styles: {
+    src: 'src/styles/*.css'
+  },
+  scripts: {
+    src: 'src/js/*.js',
+    order: ['variables.js','ParsePrintedData.js', 'SortUsers.js', 'Users.js', 'index.js']
+  },
+  html: {
+    src: 'src/pages/*.html'
+  },
+  dist: 'dist/'
+}
+
 function buildStyles() {
-  return src('./src/styles/style.css')
+  return src(paths.styles.src)
+    .pipe(concat('style.css'))
     .pipe(cleanCSS())
-    .pipe(dest('dist/'));
+    .pipe(dest(paths.dist));
 };
 
 function cleanJs() {
-  return src('src/js/*.js')
-    .pipe(order(['variables.js','ParsePrintedData.js', 'SortUsers.js', 'Users.js', 'index.js']))
-    .pipe(contact('index.js'))
+  return src(paths.scripts.src)
+    .pipe(order(paths.scripts.order))
+    .pipe(concat('index.js'))
     .pipe(uglify())
-    .pipe(dest('dist/'));
+    .pipe(dest(paths.dist));
 }
 
 function cleanHTML() {
-  return src('src/index.html')
+  return src(paths.html.src)
     .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(dest('dist/'));
+    .pipe(dest(paths.dist));
 }
 
-function watchStyle() { watch('src/styles/style.css', buildStyles)}
-function watchJS() {  watch('src/js/*.js', cleanJs)}
-function watchHTML() {  watch('src/pages/index.html', cleanHTML)}
+function watchStyle() { watch(paths.styles.src, buildStyles)}
+function watchJS() {  watch(paths.scripts.src, cleanJs)}
+function watchHTML() {  watch(paths.html.src, cleanHTML)}
 
 
 exports.build = series(buildStyles, cleanJs, cleanHTML)
